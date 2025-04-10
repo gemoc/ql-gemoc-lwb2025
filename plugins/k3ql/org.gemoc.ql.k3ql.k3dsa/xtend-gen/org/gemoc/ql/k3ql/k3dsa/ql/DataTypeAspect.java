b21634c33f8b8f5c21758cf755a35302ee21612c
Didier Vojtisek <didier.vojtisek@inria.fr>
2025-04-10 12:03:13 +0200

    update xmi

--- plugins/k3ql/org.gemoc.ql.k3ql.k3dsa/xtend-gen/org/gemoc/ql/k3ql/k3dsa/ql/DataTypeAspect.java
+++
@@ -1,7 +0,0 @@
-package org.gemoc.ql.k3ql.k3dsa.ql;
-
-import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
-
-/* @Aspect(className = _DataType) */@SuppressWarnings("all")
-public abstract class DataTypeAspect extends NamedElementAspect {
-}
